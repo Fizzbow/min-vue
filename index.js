d5b4d59a90ee45c4f9706bf9eb8775a0b2b05b7e
Fizzbow <55476631+Fizzbow@users.noreply.github.com>
2023-11-07 16:46:39 +0800

    简单实现render函数

--- index.js
+++ index.js
@@ -20,3 +20,23 @@ effectWatch(() => {
 });
 
 a.value = 30;
+
+const App = {
+  render(context) {
+    // 将响应式数据渲染到页面上
+    effectWatch(() => {
+      document.body.innerHTML = "";
+      const div = document.createElement("div");
+      div.innerText = context.state.count;
+      document.body.append(div);
+    });
+  },
+  setup() {
+    const state = reactive({
+      count: 0,
+    });
+    window.state = state;
+    return { state };
+  },
+};
+App.render(App.setup());
