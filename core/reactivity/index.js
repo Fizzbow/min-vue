4e4bda25429cd79a4bae380263a4036028147bd6
Fizzbow <55476631+Fizzbow@users.noreply.github.com>
2023-11-07 14:50:15 +0800

    feat:实现对象属性劫持

--- core/reactivity/index.js
+++ core/reactivity/index.js
@@ -39,10 +39,57 @@ const dep = new Dep(10);
 
 let b;
 effectWatch(() => {
-  // 1. 当effectWatch中传入参数,effectWatch执行这个传入的函数,并且dep.value改变的时候赋新的值set(value)
+  // 1. 当effectWatch中传入参数,effectWatch执行这个传入的函数
   b = dep.value + 20;
   console.log({ b });
 });
 
-// 2. 当dep.value发生改变的时候，通知effectWatch执行（调用notice）
+// 2. 当dep.value发生改变的时候赋新的值set(newValue)，通知effectWatch执行（调用notice）
 dep.value = 20;
+
+// 实现reactive
+// key 对应 dep
+
+const targetMap = new Map();
+
+function getDep(target, key) {
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    depsMap = new Map();
+    targetMap.set(target, depsMap);
+  }
+  let dep = depsMap.get(key);
+  if (!dep) {
+    dep = new Dep();
+    depsMap.set(key, dep);
+  }
+
+  return dep;
+}
+
+function reactive(raw) {
+  return new Proxy(raw, {
+    get(target, key) {
+      const dep = getDep(target, key);
+      dep.depend();
+
+      // Reflect.get的优点在于：会代理所有的get
+      // 下面代码等同于return target[key]
+      return Reflect.get(target, key);
+    },
+    set(target, key, value) {
+      const dep = getDep(target, key);
+      const res = Reflect.set(target, key, value);
+      dep.notice();
+      return res;
+    },
+  });
+}
+const user = reactive({ name: "sd" });
+
+let double;
+effectWatch(() => {
+  double = user.name;
+  console.log("reactive", { double });
+});
+user.name = "cssdsad";
