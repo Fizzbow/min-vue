650893180547b2d6dbcd46292634a0033eee4cfb
Fizzbow <55476631+Fizzbow@users.noreply.github.com>
2023-11-09 14:09:51 +0800

    feat:diff attribute

--- core/diff.js
+++ core/diff.js
@@ -4,5 +4,35 @@
  * @param {*} n2 newVnode
  */
 export default function diff(n1, n2) {
-  console.log({ n1 });
+  // 1.tag
+  if (n1.tag !== n2.tag) {
+    n1.el.replaceWith(document.createElement(n2.tag));
+  } else {
+    n2.el = n1.el;
+    // 2.props
+    // new: { id:"foo",class:"bar",a }
+    // old: { id:"foo",class:"bar1",a,b}
+    const { attribute: oldAttr } = n1;
+    const { attribute: newAttr } = n2;
+
+    console.log(oldAttr, newAttr);
+    if (newAttr && oldAttr) {
+      Object.keys(newAttr).forEach((key) => {
+        const newVal = newAttr[key];
+        const oldVal = oldAttr[key];
+
+        if (newVal !== oldVal) {
+          n1.el.setAttribute(key, newVal);
+        }
+      });
+    }
+
+    if (oldAttr) {
+      Object.keys(oldAttr).forEach((key) => {
+        if (!newAttr[key]) {
+          n1.el.removeAttribute(key);
+        }
+      });
+    }
+  }
 }
--- core/index.js
+++ core/index.js
@@ -5,15 +5,19 @@ export function createApp(rootComponent) {
   return {
     mounted(rootContainer) {
       const context = rootComponent.setup();
-      const isMounted = false;
+      let isMounted = false;
       let prevSubTree;
       effectWatch(() => {
         if (!isMounted) {
+          // init
           rootContainer.innerHTML = "";
           const subTree = rootComponent.render(context);
           // rootContainer.append(el);
           mountedDom(subTree, rootContainer);
+          prevSubTree = subTree;
+          isMounted = true;
         } else {
+          // updated
           const subTree = rootComponent.render(context);
           diff(prevSubTree, subTree);
           prevSubTree = subTree;
